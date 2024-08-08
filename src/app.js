198af7539ebcf10d27a5def7ff93cac86ebff7cc
baguajoe <baguajoe@gmail.com>
2024-08-08 19:46:30 +0000

    cardgen

--- src/app.js
+++ src/app.js
@@ -2,10 +2,42 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  const suits = ["&spades;", "&hearts;", "&clubs;", "&diams;"];
+  const numbers = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function getRandomCard() {
+    const randomSuits = Math.floor(Math.random() * suits.length);
+    const randomNumbers = Math.floor(Math.random() * numbers.length);
+
+    document.querySelector(".top-suit").innerHTML = suits[randomSuits];
+    document.querySelector(".number").innerHTML = numbers[randomNumbers];
+    document.querySelector(".bottom-suit").innerHTML = suits[randomSuits];
+
+    const isRed =
+      suits[randomSuits] === "&hearts;" || suits[randomSuits] === "&diams;";
+    document
+      .querySelectorAll(".top-suit, .number, .bottom-suit")
+      .forEach(element => {
+        element.style.color = isRed ? "#dc143c" : "";
+      });
+  }
+
+  getRandomCard();
+
+  document.querySelector("button").addEventListener("click", getRandomCard);
 };
